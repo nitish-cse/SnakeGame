72aa718fda4458c9761d09157d86083e04751448
Nitish Kumar Singh <96055808+nitish-cse@users.noreply.github.com>
2022-10-02 23:54:18 +0530

    first  commit

---
+++ GameFrame.java
@@ -0,0 +1,19 @@
+package FirstGame;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GameFrame(){
+//        GamePanel panel = new GamePanel();
+//        this.add(panel);
+        //by shortCut
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
---
+++ SnakeGame.java
@@ -0,0 +1,8 @@
+package FirstGame;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+         new GameFrame();
+
+    }
+}
